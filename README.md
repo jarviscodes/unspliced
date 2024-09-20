b74d182e6c3f5ebae467df8596b8e3c5d986c1bc
Cedric Sambre <cedric.sambre@protonmail.ch>
2024-09-20 13:18:16 +0200

    basic functionality ready

---
+++ README.md
@@ -0,0 +1,18 @@
+# Unspliced
+
+Sick and tired of having 5000 unsorted samples in your sound library?
+
+This tool will attempt to sort them properly according to file name, which should do the trick for MOST samples coming from Splice.
+
+# Disclaimer
+Very much WIP, don't break your projects with this :-) Contributions, PR's, Issues are welcome.
+
+Please don't use this tool if you do not  more or less understand what the code is doing.
+
+Developed and tested on Windows. Hide yo Macs (or create Issues / PR's)
+
+Currently only copies files non-destructively rather than removing them.
+
+# License 
+
+MIT
--- unspliced/__main__.py
+++ unspliced/__main__.py
@@ -1,6 +1,7 @@
 import os
 import glob
 from pathlib import Path
+import shutil
 
 # default path for splice samples is: %USERPROFILE%/Documents/Splice/Samples/packs
 
@@ -9,17 +10,20 @@ AVAILABLE_INSTRUMENTS = [
     "snare",
     "clap",
     "hat",
-    "vocal"
+    "vocal",
+    "bass",
+    "perc",
+    "songstarter",
+    "fill"
 ]
 
 def auto_detect_splice():
     print("Trying to autodetect your splice folder")
-    if os.name == "nt":
-        home_path = Path.home()
-        guessed_path = home_path / "Documents/Splice/Samples/packs"
-        if guessed_path.exists():
-            print("Found splice packs under: ", guessed_path)
-            return guessed_path
+    home_path = Path.home()
+    guessed_path = home_path / "Documents/Splice/Samples/packs"
+    if guessed_path.exists():
+        print("Found splice packs under: ", guessed_path)
+        return guessed_path
 
 def find_instrument(file_stem):
     tmp_stem = file_stem.lower()
@@ -36,6 +40,35 @@ def dive_until_ext(extension: str = ".wav"):
     return all_sound_files
 
 
+def create_file_structure(root="/"):
+    new_sample_path = Path(root) /  "samples"
+    if not new_sample_path.exists():
+        new_sample_path.mkdir()
+
+    for instrument in AVAILABLE_INSTRUMENTS:
+        instrument_path = new_sample_path / instrument
+        if not instrument_path.exists():
+            instrument_path.mkdir()
+
+    # create "other" dir
+    other_dir = new_sample_path / "other"
+    if not other_dir.exists():
+        other_dir.mkdir()
+
+    return new_sample_path
+
+def main():
+    new_root = create_file_structure()
+    all_sound_files = dive_until_ext()
+    for sound_file in all_sound_files:
+        stem = Path(sound_file).stem
+        instrument = find_instrument(stem)
+
+        if not instrument:
+            print("Could not find instrument for: ", stem)
+            instrument = "other"
+
+        shutil.copy(Path(sound_file), new_root / instrument)
 
 
-all_sound_files = dive_until_ext()
+main()
